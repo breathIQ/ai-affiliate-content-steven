6541c58b26491eac923bc2d497e4c098681916fc
Sonu-cvinfotech <sonu@cvinfotech.com>
2026-02-16 18:09:20 +0530

    invalid affiliate url

--- app/Http/Controllers/Api/v1/User/AffiliateClickController.php
+++ app/Http/Controllers/Api/v1/User/AffiliateClickController.php
@@ -127,7 +127,7 @@ class AffiliateClickController extends ResponseController
         $user = User::where('affiliate_id', $affiliate_id)->firstOrFail();
 
         if(!$user){
-            return $this->sendError('Invalid affiliate id');
+            return $this->sendError('Invalid affiliate url');
         }
         // 1. Detect Platform from URL parameter OR Referrer header
         $platform = $request->query('ref') ?? $this->parseReferrer($request->header('referer'));
